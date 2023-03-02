70a64a62cc9d763890ebade275ef567fd163fdc3
Hyeoncheol Nam <hyeoncheol.nam@zeroum.co.jp>
2023-03-03 00:54:54 +0900

    Initial commit

---
+++ edit_token/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['simsu'] = user.id
+        # ...
+
+        return token
